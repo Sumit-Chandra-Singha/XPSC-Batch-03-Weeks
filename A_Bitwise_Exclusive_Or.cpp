b520a29f62c0375480ecab24bfe442e2f8e8ab6b
Sumit Chandra Singha <129593007+Sumit-Chandra-Singha@users.noreply.github.com>
2023-10-16 23:47:36 +0600

    Add files via upload

---
+++ A_Bitwise_Exclusive_Or.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for(int i=a; i<b; i++)
+
+int solveFunction()
+{
+    ll n,m;
+    cin >> n >> m;
+    ll ans = n ^ m;
+    cout << ans;
+
+    cout<<"\n";
+    return 0;
+}
+
+int main()
+{
+    fio;
+    // int t;
+    // cin>>t;
+    // while(t--)
+        solveFunction();
+
+    return 0;
+}
