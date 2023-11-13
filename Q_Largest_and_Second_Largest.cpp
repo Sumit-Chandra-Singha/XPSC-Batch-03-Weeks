9cd27e484d7ff40992091c2776a2800dcb8c2b18
Sumit Chandra Singha <129593007+Sumit-Chandra-Singha@users.noreply.github.com>
2023-11-14 00:24:34 +0600

    Add files via upload

---
+++ Q_Largest_and_Second_Largest.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for(int i=a; i<b; i++)
+#define nl cout<<"\n";
+
+int solveFunction()
+{
+    ll n;
+    cin >> n;
+    ll ar[n];
+    loop(i, 0, n) cin >> ar[i];
+    sort(ar, ar + n);
+    cout<<ar[n-1]+ar[n-2]<<"\n";
+    return 0;
+}
+
+int main()
+{
+    fio;
+    int t;
+    cin>>t;
+    while(t--)
+        solveFunction();
+
+    return 0;
+}
