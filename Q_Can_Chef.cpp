286212d167168aedcdfd43c6e2e87a3121f7c20c
Sumit Chandra Singha <129593007+Sumit-Chandra-Singha@users.noreply.github.com>
2023-12-01 23:53:54 +0600

    Add files via upload

---
+++ Q_Can_Chef.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for(int i=a; i<b; i++)
+#define nl cout<<"\n";
+
+
+int solveFunction()
+{
+    ll n,m;
+    cin >> n>>m;
+    
+    if(n*15>=2*m){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+
+    cout<<"\n";
+    return 0;
+}
+
+int main()
+{
+    fio;
+    int t;
+    cin>>t;
+    while(t--)
+        solveFunction();
+
+    return 0;
+}
